745bb0eb7ba28e6993502c8c44c371ebf2e437a7
Anton Karmanov <antkar.dev@gmail.com>
2014-04-18 19:51:47 +0200

    Allow to specify the amount of memory available to the script.

--- sample/core/sample.cpp
+++ sample/core/sample.cpp
@@ -57,28 +57,47 @@ namespace {
 		return gc::create<ss::String>(out.str());
 	}
 
-	gc::Local<ss::StringArray> create_arguments_array(const std::vector<const char*>& arguments_c) {
-		std::size_t n = arguments_c.size();
+	gc::Local<ss::StringArray> create_arguments_array(const std::vector<std::string>& arguments_std) {
+		std::size_t n = arguments_std.size();
 		gc::Local<ss::StringArray> array = ss::StringArray::create(n);
-		for (std::size_t i = 0; i < n; ++i) (*array)[i] = gc::create<ss::String>(arguments_c[i]);
+		for (std::size_t i = 0; i < n; ++i) (*array)[i] = gc::create<ss::String>(arguments_std[i]);
 		return array;
+	}
+
+	std::size_t get_effective_memory_limit(std::size_t limit_mb) {
+		if (limit_mb == 0) limit_mb = 8;
+		if (limit_mb > 2048) limit_mb = 2048;
+
+		std::size_t limit = limit_mb << 20;
+		try {
+			char* test_data = new char[limit];
+			free(test_data);
+		} catch (std::bad_alloc) {
+			throw ss::FatalError("Not enough memory");
+		}
+
+		return limit;
 	}
 }//namespace
 
 void link__api();
 
-int sample_main(const char* file_name_c, const std::vector<const char*>& arguments_c) {
+int sample_main(
+	const std::string& file_name_std,
+	const std::vector<std::string>& arguments_std,
+	std::size_t mem_limit_mb)
+{
 	link__api();
 
 	try {
-		gc::startup_guard gc_startup(8 * 1024 * 1024);
+		gc::startup_guard gc_startup(get_effective_memory_limit(mem_limit_mb));
 		gc::manage_thread_guard gc_thread;
 		gc::enable_guard gc_enable;
 
 		try {
-			ss::StringLoc file_name = gc::create<ss::String>(file_name_c);
-			ss::StringLoc code = load_file(file_name_c);
-			gc::Local<ss::StringArray> arguments = create_arguments_array(arguments_c);
+			ss::StringLoc file_name = gc::create<ss::String>(file_name_std);
+			ss::StringLoc code = load_file(file_name_std);
+			gc::Local<ss::StringArray> arguments = create_arguments_array(arguments_std);
 			gc::Local<gc::Array<rt::ScriptSource>> sources = rt::get_single_script_source(file_name, code);
 			bool ok = rt::execute_top_script(sources, arguments);
 			return ok ? 0 : 1;
--- sample/main/main.cpp
+++ sample/main/main.cpp
@@ -16,21 +16,56 @@
 
 //Script Language interpreter application entry point.
 
+#include <cstring>
 #include <iostream>
+#include <stdexcept>
+#include <string>
 #include <vector>
 
-int sample_main(const char* file_name, const std::vector<const char*>& arguments);
+int sample_main(
+	const std::string& file_name,
+	const std::vector<std::string>& arguments,
+	std::size_t mem_limit_mb);
 
-int main(int argc, const char** argv) {
-	if (argc < 2) {
-		std::cerr << "Usage: " << argv[0] << " FILE <arguments>\n";
+namespace {
+	int command_line_error() {
+		std::cerr << "Usage: script [-m MEMORY_LIMIT_MB] FILE (ARGUMENT)*\n";
 		return 1;
 	}
+}
 
-	const char* file_name = argv[1];
-	
-	std::vector<const char*> arguments;
-	for (int i = 2; i < argc; ++i) arguments.push_back(argv[i]);
+int main(int argc, const char** argv) {
+	std::size_t mem_limit = 0;
 
-	sample_main(file_name, arguments);
+	int argpos = 1;
+
+	if (argpos < argc && 0 == strcmp("-m", argv[argpos])) {
+		++argpos;
+		if (argpos == argc) return command_line_error();
+
+		std::string limit_str = argv[argpos++];
+
+		int v;
+		try {
+			v = std::stoi(limit_str);
+		} catch (std::logic_error&) {
+			std::cerr << "Invalid memory limit\n";
+			return 1;
+		}
+		
+		if (v < 1 || v > 2048) {
+			std::cerr << "Memory limit is out of range\n";
+			return 1;
+		}
+
+		mem_limit = v;
+	}
+
+	if (argpos == argc) return command_line_error();
+
+	const std::string file_name = argv[argpos++];
+	std::vector<std::string> arguments;
+	while (argpos < argc) arguments.push_back(argv[argpos++]);
+
+	sample_main(file_name, arguments, mem_limit);
 }
